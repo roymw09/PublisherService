6ae40ab6f2653d3792bc8c81c84ae59c5a2e18c4
roymw09 <roymw09@gmail.com>
2022-03-14 14:55:07 -0700

    Added Eureka dependency

--- src/main/java/org/ac/cst8277/williams/roy/PublishServiceApplication.java
+++ src/main/java/org/ac/cst8277/williams/roy/PublishServiceApplication.java
@@ -2,7 +2,6 @@ package org.ac.cst8277.williams.roy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 public class PublishServiceApplication {
