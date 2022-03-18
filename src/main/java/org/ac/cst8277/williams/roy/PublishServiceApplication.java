459ff70c1f90de51aa03a0db0b146ad51337c2e1
roymw09 <roymw09@gmail.com>
2022-03-18 06:01:27 -0700

    Removed Eureka dependency

--- src/main/java/org/ac/cst8277/williams/roy/PublishServiceApplication.java
+++ src/main/java/org/ac/cst8277/williams/roy/PublishServiceApplication.java
@@ -3,7 +3,6 @@ package org.ac.cst8277.williams.roy;
 import org.ac.cst8277.williams.roy.model.Content;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
@@ -11,11 +10,8 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-@EnableScheduling
 public class PublishServiceApplication {
 
     public static void main(String[] args) {
