76e444e4b7919db376baab213aa13c0098a10357
roywilliams <roymw09@gmail.com>
2022-08-04 22:26:16 -0230

    Updated database table schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,12 @@
 DROP TABLE IF EXISTS content;
 DROP TABLE IF EXISTS publisher;
 CREATE TABLE publisher (
-    id VARCHAR(250) PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     user_id INT NOT NULL UNIQUE
 );
 CREATE TABLE content (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     publisher_id INT NOT NULL,
     content VARCHAR(500) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (publisher_id) REFERENCES publisher(user_id)
+    FOREIGN KEY (publisher_id) REFERENCES publisher(id)
 );
